73e9f6afcd981e52e6407b2c1e3f7b53b64fa061
Sinthorn Pradutnam <psinthorn@imacs-iMac-2.local>
2021-04-05 11:42:28 +0700

    Mockup locations

--- src/components/mainview/destinations/locations/Locations.js
+++ src/components/mainview/destinations/locations/Locations.js
@@ -1,6 +1,54 @@
-import * as s from './Locations.styles';
+// import * as s from './Locations.styles';
 
-const Locations = () => {
+const Locations = (props) => {
+    const location = props.match.params.location;
+    console.log(location);
+
+    const locations = {
+        chaweng: {
+            title: "Chaweng",
+            desc: "Chaweng beach is most beutiful beach of Samui",
+            imf: "",
+            category: "travel",
+            created_at: "",
+            status: true
+        },
+        lamai: {
+            title: "Lamai",
+            desc: "Lamai beach is most beutiful beach of Samui",
+            imf: "",
+            category: "travel",
+            created_at: "",
+            status: true
+        },
+        bophut: {
+            title: "Bophut",
+            desc: "Bophut beach is most beutiful beach of Samui",
+            imf: "",
+            category: "travel",
+            created_at: "",
+            status: true
+        },
+        plaileam: {
+            title: "Plaileam",
+            desc: "Plaileam beach is most beutiful beach of Samui",
+            imf: "",
+            category: "travel",
+            created_at: "",
+            status: true
+        },
+        choengmon: {
+            title: "Choengmon",
+            desc: "Choengmon beach is most beutiful beach of Samui",
+            imf: "",
+            category: "travel",
+            created_at: "",
+            status: true
+        }
+        
+    }
+
+
     return (
         <div>
             <h2>Locations Link Page</h2>
